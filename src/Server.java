d2e1fd681c9003c6962a0ad2cb6ab1fedc66ca25
shreyansh <shreyansh@localhost.localdomain>
2019-08-12 11:55:56 +0530

    Server code almost created

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,44 @@
+import java.io.*;
+import java.awt.*;
+import java.net.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Server {
+public class Server extends JFrame
+{
+	private JTextField userText;
+	private JTextArea chatWindow;
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+	private ServerSocket server;
+	private Socket connection;
+	
+	public Server()
+	{
+		super("WarDaddy Messenger");
+		userText = new JTextField();
+		userText.setEditable(false);
+		userText.addActionListener(
+				
+				new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+							sendMessage(e.getActionCommand());
+							userText.setText("");
+					}
+				});
+		add(userText, BorderLayout.NORTH);
+		chatWindow = new JTextArea();
+		add(new JScrollPane(chatWindow));
+		setSize(500,500);
+		setVisible(true);
+		
+		
+		
+		
+	}
+	
+	
 
 }
